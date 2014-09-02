b2b394b2f24a1fb2717f1b394d5183e688d673bd
Gustavo Zambonin <gzmbnn@gmail.com>
2014-09-02 11:15:13 -0300

    Update and rename ExcecaoEstruturaCheia.h to ExcecaoListaCheia.h

--- Lista/ExcecaoListaCheia.h
+++ Lista/ExcecaoListaCheia.h
@@ -1,19 +1,19 @@
 //! Copyright year [2014] <Gustavo Zambonin & Lucas Ribeiro Neis>
-//! Classe de exceção para estrutura cheia.
-/*! Uma classe que trata exceções quando a estrutura linear
+//! Classe de exceção para lista cheia.
+/*! Uma classe que trata exceções quando a lista
 *	está com seu número máximo de elementos.
 *	\author Gustavo Zambonin, Lucas Ribeiro Neis
-*	\since 28/08/14
+*	\since 02/09/14
 *	\version 1.0
 */
 
-#ifndef EXCECAOESTRUTURACHEIA_H_
-#define EXCECAOESTRUTURACHEIA_H_
+#ifndef EXCECAOLISTACHEIA_H_
+#define EXCECAOLISTACHEIA_H_
 #include <exception>
 
 using namespace std;
 
-class ExcecaoEstruturaCheia : public exception {
+class ExcecaoListaCheia : public exception {
  public:
     //! Método responsável por descrever o erro relacionado à exceção.
  	/*! 
@@ -21,7 +21,7 @@ class ExcecaoEstruturaCheia : public exception {
  	*/
 
 	virtual const char* what() const throw() {
-		return("Sua estrutura linear está cheia!");
+		return("Sua lista está cheia!");
 	}
 };
 
