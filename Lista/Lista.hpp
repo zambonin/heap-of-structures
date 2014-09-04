2b7dbbb6e8099a2f24d4115cad163f1c4ebc85b9
Gustavo Zambonin <gzmbnn@gmail.com>
2014-09-04 13:37:04 -0300

    modified:   Lista/ExcecaoDadoNaoEncontrado.h 	modified:   Lista/ExcecaoPosicao.h 	deleted:    Lista/Lista 	modified:   Lista/Lista.hpp 	deleted:    Lista/Lista.hpp.gch 	deleted:    Lista/lista 	deleted:    Lista/main 	deleted:    Lista/main.cpp

--- Lista/Lista.hpp
+++ Lista/Lista.hpp
@@ -1,44 +1,164 @@
+//! Copyright year [2014] <Gustavo Zambonin & Lucas Ribeiro Neis>
+//! Classe que descreve a lista.
+/*! Classe que trata das operações relacionadas a listas.
+*	\author Gustavo Zambonin, Lucas Ribeiro Neis
+*	\since 03/09/14
+*	\version 1.0
+*/
+
 #ifndef LISTA_HPP_
 #define LISTA_HPP_
+#define MAX 100
 #include "ExcecaoListaVazia.h"
 #include "ExcecaoListaCheia.h"
 #include "ExcecaoDadoNaoEncontrado.h"
 #include "ExcecaoPosicao.h"
-#define MAX 100
 
 template <typename T>
 class Lista {
- public:
+ protected:
+ 	//! Vetor básico.
+ 	/*! Vetor usado para armazenar os dados da lista. */
  	T* lista;
- 	int tamanho;
+
+ 	//! Identificador de posição.
+ 	/*! Posição do último elemento da lista. */
  	int ultimo;
 
- 	Lista();											//ok
- 	Lista(int t);										//ok
+ 	//! Identificador de tamanho.
+ 	/*! Tamanho da lista. */
+ 	int tamanho;
 
- 	T adiciona(T dado);									//ok
- 	T adicionaNoInicio(T dado);							//ok
- 	T adicionaNaPosicao(T dado, int posicao);			//ok
- 	T adicionaEmOrdem(T dado);							//precisa do algoritmo do professor
+ 	// POR QUE O ADICIONA RETORNA UM TIPO GENÉRICO????? que doença wtf
 
- 	T retira();											//ok
- 	T retiraDoInicio();									//ok
- 	T retiraDaPosicao(int posicao);						//ok
- 	T retiraEspecifico(T dado);							//ok
+ public:
+ 	//! Construtor.
+    /*! Construtor básico para a classe, sem parâmetros. */
+ 	Lista();
 
- 	bool listaCheia();									//ok
- 	bool listaVazia();									//ok
+ 	//! Construtor com parâmetros.
+ 	/*! Construtor que permite a variabilidade de tamanho da lista. 
+ 	* \param t o número máximo de elementos no vetor.
+ 	* \sa Lista()
+ 	*/
+ 	explicit Lista(int t);
 
- 	int posicao(T dado);								//ok
- 	bool contem(T dado);								//ok
+ 	//! Método para adicionar dados à lista.
+ 	/*!
+ 	* \param dado um objeto genérico que representa o dado a ser adicionado.
+ 	* \sa adicionaNoInicio(), adicionaNaPosicao(), adicionaEmOrdem(),
+ 	* retira(), retiraDoInicio(), retiraDaPosicao(), retiraEspecifico()
+ 	*/
+ 	T adiciona(T dado);
 
- 	bool maior(T dado1, T dado2);						//precisa algoritmo
- 	bool menor(T dado1, T dado2);						//same
- 	bool igual(T dado1, T dado2);						//same
+ 	//! Método para adicionar um dado diretamente no início da lista.
+ 	/*!
+ 	* \param dado um objeto genérico que representa o dado a ser adicionado.
+ 	* \sa adiciona(), adicionaNaPosicao(), adicionaEmOrdem(), retira(),
+ 	* retiraDoInicio(), retiraDaPosicao(), retiraEspecifico()
+ 	*/
+ 	T adicionaNoInicio(T dado);
 
- 	bool posicaoInvalida(int p);						//ok
- 	void inicializaLista();								//ok
- 	void destroiLista();								//ok
+ 	//! Método para adicionar um dado em uma posição específica.
+ 	/*!
+ 	* \param dado um objeto genérico que representa o dado a ser adicionado.
+ 	* \param posicao a posição desejada na forma de número inteiro.
+ 	* \sa adiciona(), adicionaNoInicio(), adicionaEmOrdem(), retira(),
+ 	* retiraDoInicio(), retiraDaPosicao(), retiraEspecifico()
+ 	*/
+ 	T adicionaNaPosicao(T dado, int posicao);
+
+ 	//! no fucking idea
+ 	T adicionaEmOrdem(T dado); //precisa do professor
+
+ 	//! Método para retirar dados da lista.
+ 	/*! 
+ 	* \return um objeto genérico que era o último da lista.
+ 	* \sa adiciona(), adicionaNoInicio(), adicionaNaPosicao(), 
+ 	* adicionaEmOrdem(), retiraDoInicio(), retiraDaPosicao(), 
+ 	* retiraEspecifico()
+ 	*/
+ 	T retira();
+
+ 	//! Método para retirar o primeiro dado da lista.
+ 	/*!
+ 	* \return um objeto genérico que era o primeiro da lista.
+	* \sa adiciona(), adicionaNoInicio(), adicionaNaPosicao(), 
+ 	* adicionaEmOrdem(), retira(), retiraDaPosicao(), retiraEspecifico()
+ 	*/
+ 	T retiraDoInicio();
+
+ 	//! Método para retirar um dado em uma posição específica.
+ 	/*!
+ 	* \param posicao a posição desejada na forma de número inteiro.
+ 	* \return um objeto genérico que tinha como índice a posição indicada.
+ 	* \sa adiciona(), adicionaNoInicio(), adicionaNaPosicao(), 
+ 	* adicionaEmOrdem(), retiraDoInicio(), retiraEspecifico()
+ 	*/
+ 	T retiraDaPosicao(int posicao);
+
+ 	//! Método para retirar um dado específico.
+ 	/*!
+ 	* \param dado o dado a ser informado para comparação no vetor.
+ 	* \return o dado encontrado no vetor de acordo com a comparação.
+ 	* \sa adiciona(), adicionaNoInicio(), adicionaNaPosicao(), 
+ 	* adicionaEmOrdem(), retiraDoInicio(), retiraDaPosicao(), 
+ 	*/
+ 	T retiraEspecifico(T dado);
+
+ 	//! Método que mostra se a lista está cheia.
+	/*!
+	* \return um boolean.
+	* \sa listaVazia()
+	*/
+ 	bool listaCheia();
+
+ 	//! Método que mostra se a lista está vazia.
+	/*!
+	* \return um boolean.
+	* \sa listaCheia()
+	*/
+ 	bool listaVazia();
+
+ 	//! Método para identificar a posição de um dado específico no vetor.
+ 	/*!
+ 	* \param dado o dado a ser informado para comparação no vetor.
+ 	* \return a posição do dado indicado.
+ 	* \sa contem()
+ 	*/
+ 	int posicao(T dado);
+
+ 	//! Método para identificar a presença de um dado específico no vetor.r
+ 	/*!
+ 	* \param dado o dado a ser informado para comparação no vetor.
+ 	* \return um boolean.
+ 	* \sa posicao()
+ 	*/
+ 	bool contem(T dado);
+
+ 	bool maior(T dado1, T dado2); //same
+ 	bool menor(T dado1, T dado2); //same
+ 	bool igual(T dado1, T dado2); //same
+
+ 	//! Método para checagem de posição inválida solicitada no vetor.
+ 	/*!
+ 	* \param p a posição a ser validada.
+ 	* \return um boolean.
+ 	* \sa posicao()
+ 	*/
+ 	bool posicaoInvalida(int p);
+
+ 	//! Método que inicializa a lista sem elementos.
+	/*!
+	* \sa destroiLista()
+	*/
+ 	void inicializaLista();
+
+ 	//! Método que destrói a lista.
+ 	/*!
+ 	* \sa inicializaLista()
+ 	*/
+ 	void destroiLista();
 };
 
 template <typename T>
