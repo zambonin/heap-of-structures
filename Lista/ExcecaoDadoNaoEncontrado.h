cb7b21665ac41203ef41b8918e39c0d4d838776d
Gustavo Zambonin <gzmbnn@gmail.com>
2014-09-04 13:37:04 -0300

    vector: add some documentation to list structure

--- Lista/ExcecaoDadoNaoEncontrado.h
+++ Lista/ExcecaoDadoNaoEncontrado.h
@@ -1,3 +1,12 @@
+//! Copyright year [2014] <Gustavo Zambonin & Lucas Ribeiro Neis>
+//! Classe de exceção para dados inexistentes em estruturas.
+/*! Uma classe que trata exceções quando a lista
+*	não encontra o elemento solicitado.
+*	\author Gustavo Zambonin, Lucas Ribeiro Neis
+*	\since 03/09/14
+*	\version 1.0
+*/
+
 #ifndef EXCECAODADONAOENCONTRADO_H_
 #define EXCECAODADONAOENCONTRADO_H_
 #include <exception>
--- Lista/ExcecaoPosicao.h
+++ Lista/ExcecaoPosicao.h
@@ -1,3 +1,12 @@
+//! Copyright year [2014] <Gustavo Zambonin & Lucas Ribeiro Neis>
+//! Classe de exceção para posições inválidas em estruturas.
+/*! Uma classe que trata exceções quando a lista 
+*	tenta manipular um elemento em uma posição inexistente.
+*	\author Gustavo Zambonin, Lucas Ribeiro Neis
+*	\since 03/09/14
+*	\version 1.0
+*/
+
 #ifndef EXCECAOPOSICAO_H_
 #define EXCECAOPOSICAO_H_
 #include <exception>
