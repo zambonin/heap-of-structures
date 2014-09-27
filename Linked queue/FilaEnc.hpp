f07f2b7b7f63679e1948afcdd314408d8b13f6b8
Gustavo Zambonin <gzmbnn@gmail.com>
2014-09-27 18:28:19 -0300

    linked: add docs to linked stack and queue

--- Linked queue/FilaEnc.hpp
+++ Linked queue/FilaEnc.hpp
@@ -1,3 +1,11 @@
+//! Copyright year [2014] <Gustavo Zambonin & Lucas Ribeiro Neis>
+//! Classe que descreve a fila encadeada.
+/*! Classe que trata das operações relacionadas a filas encadeadas. 
+*	\author Gustavo Zambonin, Lucas Ribeiro Neis
+*	\since 27/09/14
+*	\version 1.0
+*/
+
 #ifndef FILAENC_HPP_
 #define FILAENC_HPP_
 #include "ListaEnc.hpp"
@@ -5,18 +13,59 @@
 #include "ExcecaoListaVazia.h"
 
 template <typename T>
-class FilaEnc : ListaEnc<T> {
+class FilaEnc : private ListaEnc<T> {
  private:
+    //! Fim da fila encadeada.
+    /*! Ponteiro que aponta para o último elemento da fila encadeada. */
     Elemento<T>* fim;
 
  public:
-    FilaEnc<T>();
+    //! Construtor.
+	/*! Construtor para a classe, sem parâmetros. Herdado da classe-pai. */
+    FilaEnc();
+
+    //! Destrutor.
+	/*! Destrutor herdado da classe-pai, que deleta todos os elementos. */
     ~FilaEnc();
+
+    //! Método para adicionar um dado à fila encadeada.
+	/*!
+	* \param dado o endereço do objeto genérico a ser adicionado.
+	* \sa retira()
+	*/
     void inclui(const T& dado);
+
+    //! Método para retirar o último dado da fila encadeada.
+	/*!
+	* \return um objeto genérico que era o último da fila encadeada.
+	* \sa inclui()
+	*/
     T retira();
+
+    //! Método de identificação do último elemento da fila encadeada.
+	/*!
+	* \return o último objeto adicionado à fila encadeada.
+	* \sa primeiro()
+	*/
     T ultimo();
+
+    //! Método de identificação do primeiro elemento da fila encadeada.
+	/*!
+	* \return o primeiro objeto adicionado à fila encadeada.
+	* \sa ultimo()
+	*/
     T primeiro();
+
+    //! Método que mostra se a fila está vazia.
+	/*!
+	* \return um boolean.
+	*/
     bool filaVazia();
+
+    //! Método para eliminar iterativamente cada elemento da fila encadeada.
+    /*!
+    * \sa ~FilaEnc()
+    */
     void limparFila();
 };
 
@@ -33,10 +82,10 @@ FilaEnc<T>::~FilaEnc() {
 template<typename T>
 void FilaEnc<T>::inclui(const T& dado) {
 	Elemento<T>* novo = new Elemento<T>(dado, 0);
-	if(novo == 0) {
+	if (novo == 0) {
 		throw ExcecaoListaCheia();
 	}
-	if(filaVazia()) {
+	if (filaVazia()) {
 		this->cabeca = novo;
 	} else {
 		fim->setProximo(novo);
@@ -54,7 +103,7 @@ T FilaEnc<T>::retira() {
 	Elemento<T>* sai = this->cabeca;
 	T volta = sai->getInfo();
 	this->cabeca = sai->getProximo();
-	if(this->tamanho == 1) {
+	if (this->tamanho == 1) {
 		fim = 0;
 	}
 	this->tamanho--;
--- Linked stack/ListaEnc.hpp
+++ Linked stack/ListaEnc.hpp
@@ -1,4 +1,11 @@
 //! Copyright year [2014] <Gustavo Zambonin & Lucas Ribeiro Neis>
+//! Classe que descreve a lista encadeada.
+/*! Classe que trata das operações relacionadas a listas encadeadas. 
+*	\author Gustavo Zambonin, Lucas Ribeiro Neis
+*	\since 14/09/14
+*	\version 1.0
+*/
+
 #ifndef LISTAENC_HPP_
 #define LISTAENC_HPP_
 #include "Elemento.hpp"
@@ -10,29 +17,172 @@
 template<typename T>
 class ListaEnc {
  protected:
+    //! Cabeça da lista encadeada.
+    /*! Elemento que descreve o primeiro nó da lista encadeada. */
 	Elemento<T>* cabeca;
+
+	//! Identificador de tamanho.
+	 /*! Tamanho da lista encadeada. */
 	int tamanho;
 
  public:
+    //! Construtor.
+    /*! Construtor básico para a classe, sem parâmetros. */
 	ListaEnc();
+
+	//! Destrutor.
+	/*! Destrutor que deleta todos os nós. */
 	~ListaEnc();
+
+	//! Método para adicionar um dado diretamente no fim da lista encadeada.
+	/*!
+	* \param dado o endereço do objeto genérico a ser adicionado.
+	* \sa adicionaEmOrdem(), adicionaNaPosicao(), adicionaNoInicio(),
+	* eliminaDoInicio(), retira(), retiraEspecifico(), retiraDaPosicao(),
+	* retiraDoInicio()
+	*/
 	void adiciona(const T& dado);
+
+	//! Método para adicionar um dado em uma posição específica
+	//! de acordo com a ordem inerente da classe.
+	/*!
+	* \param dado o endereço do objeto genérico a ser adicionado.
+	* \sa adiciona(), adicionaNaPosicao(), adicionaNoInicio(),
+	* eliminaDoInicio(), retira(), retiraEspecifico(), retiraDaPosicao(),
+	* retiraDoInicio()
+	*/
 	void adicionaEmOrdem(const T& dado);
+
+	//! Método para adicionar um dado em uma posição específica.
+	/*!
+ 	* \param dado o endereço do objeto genérico a ser adicionado.
+	* \param pos a posição desejada na forma de número inteiro.
+	* \sa adiciona(), adicionaEmOrdem(), adicionaNoInicio(), retira(),
+	* retiraDaPosicao(), retiraDoInicio(), retiraEspecifico()
+	*/
 	void adicionaNaPosicao(const T& dado, int pos);
+
+	//! Método para adicionar um dado diretamente no início da lista encadeada.
+	/*!
+	* \param dado o endereço do objeto genérico a ser adicionado.
+	* \sa adiciona(), adicionaEmOrdem(), adicionaNaPosicao(), retira(),
+	* retiraDaPosicao(), retiraDoInicio(), retiraEspecifico()
+	*/
 	void adicionaNoInicio(const T& dado);
+
+	//! Método para eliminar o primeiro nó da lista encadeada.
+	/*!
+	* \param dado o endereço do objeto genérico a ser adicionado.
+	* \sa ~ListaEnc()
+	*/
 	void eliminaDoInicio();
+
+	//! Método para retirar dados da lista encadeada.
+	/*!
+	* \return um objeto genérico que era o último da lista.
+	* \sa adiciona(), adicionaEmOrdem(), adicionaNaPosicao(), adicionaNoInicio()
+	* retiraDaPosicao(), retiraDoInicio(), retiraEspecifico()
+	*/
 	T retira();
-	T retiraEspecifico(const T& dado);
+
+	//! Método para retirar um dado em uma posição específica.
+ 	/*!
+ 	* \param pos a posição desejada na forma de número inteiro.
+ 	* \return um objeto genérico que tinha como índice a posição indicada.
+ 	* \sa adiciona(), adicionaEmOrdem(), adicionaNaPosicao(), 
+ 	* adicionaNoInicio(), retira(), retiraDoInicio(), retiraEspecifico()
+ 	*/
 	T retiraDaPosicao(int pos);
+
+	//! Método para retirar o primeiro dado da lista.
+ 	/*!
+ 	* \return um objeto genérico que era o primeiro da lista.
+	* \sa adiciona(), adicionaEmOrdem(), adicionaNaPosicao(), 
+ 	* adicionaNoInicio(), retira(), retiraDaPosicao(), retiraEspecifico()
+ 	*/
 	T retiraDoInicio();
+
+	//! Método para retirar um dado específico.
+ 	/*!
+ 	* \param dado o endereço do objeto genérico a ser adicionado.
+ 	* \return o dado encontrado no vetor de acordo com a comparação.
+ 	* \sa adiciona(), adicionaNoInicio(), adicionaNaPosicao(), 
+ 	* adicionaEmOrdem(), retira(), retiraDaPosicao(), retiraDoInicio()
+ 	*/
+	T retiraEspecifico(const T& dado);
+
+	//! Método para identificar a presença de um dado
+	//! específico na lista encadeada.
+ 	/*!
+ 	* \param dado o endereço do objeto genérico a ser comparado.
+ 	* \return um boolean.
+ 	* \sa posicao(), posicaoMem()
+ 	*/
 	bool contem(const T& dado);
+
+	//! Método para identificar a presença de um dado
+	//! específico na lista encadeada.
+ 	/*!
+ 	* \param dado o endereço do objeto genérico a ser comparado.
+ 	* \return um boolean.
+ 	* \sa contem(), posicaoMem()
+ 	*/
 	int posicao(const T& dado) const;
+
+	//! Método para retornar a posição na memória de um dado
+	//! especfico na lista encadeada.
+	/*!
+	* \param dado o endereço do objeto genérico a ser comparado.
+	* \return um ponteiro relacionado ao dado específico.
+	* \sa contem(), posicao()
+	*/
 	T* posicaoMem(const T& dado) const;
+
+	//! Método para eliminar iterativamente cada elemento da lista encadeada.
+	/*!
+	* \sa ~ListaEnc()
+	*/
 	void destroiLista();
+
+	//! Método que mostra se a lista está vazia.
+	/*!
+	* \return um boolean.
+	* \sa listaCheia()
+	*/
 	bool listaVazia() const;
-	bool igual(T dado1, T dado2);
+
+	//! Método de comparação de igualdade entre dois dados.
+    /*!
+	* /param dado1 o primeiro dado a ser comparado.
+	* /param dado2 o segundo dado a ser comparado.
+	* \sa maior(), menor()
+	*/
+    bool igual(T dado1, T dado2);
+
+    //! Método de comparação de maioridade (de acordo com um critério
+    //! estabelecido pela classe) entre dois dados.
+    /*!
+    * /param dado1 o primeiro dado a ser comparado.
+    * /param dado2 o segundo dado a ser comparado.
+    * \sa igual(), menor()
+    */
 	bool maior(T dado1, T dado2);
+
+    //! Método de comparação de minoridade (de acordo com um critério
+    //! estabelecido pela classe) entre dois dados.
+	/*!
+	* /param dado1 o primeiro dado a ser comparado.
+	* /param dado2 o segundo dado a ser comparado.
+	* \sa igual(), maior()
+	*/
 	bool menor(T dado1, T dado2);
+
+	 //! Método para checagem de posição inválida solicitada no vetor.
+ 	/*!
+ 	* \param p a posição a ser validada.
+ 	* \return um boolean.
+ 	* \sa posicao(), posicaoMem()
+ 	*/
 	bool posicaoInvalida(int p);
 };
 
@@ -75,8 +225,7 @@ void ListaEnc<T>::adicionaNaPosicao(const T& dado, int posicao) {
 		throw ExcecaoPosicao();
 	}
 	if (posicao == 0) {
-		adicionaNoInicio(dado);
-		return;
+		return adicionaNoInicio(dado);
 	}
 	Elemento<T>* novo = new Elemento<T>(dado, 0);
 	if (novo == 0) {
@@ -119,14 +268,6 @@ T ListaEnc<T>::retira() {
 }
 
 template <typename T>
-T ListaEnc<T>::retiraEspecifico(const T& dado) {
-	if(listaVazia()) {
-		throw ExcecaoListaVazia();
-	}
-	return retiraDaPosicao(posicao(dado) + 1);
-}
-
-template <typename T>
 T ListaEnc<T>::retiraDaPosicao(int posicao) {
 	if (posicaoInvalida(posicao)) {
 		throw ExcecaoPosicao();
@@ -139,7 +280,7 @@ T ListaEnc<T>::retiraDaPosicao(int posicao) {
 		anterior = anterior->getProximo();
 	}
 	Elemento<T>* eliminar = anterior->getProximo();
-    T volta = eliminar->getInfo();
+	T volta = eliminar->getInfo();
 	anterior->setProximo(eliminar->getProximo());
 	tamanho--;
 	delete eliminar;
@@ -148,17 +289,23 @@ T ListaEnc<T>::retiraDaPosicao(int posicao) {
 
 template <typename T>
 T ListaEnc<T>::retiraDoInicio() {
-	Elemento<T>* saiu;
-	T volta;
 	if (listaVazia()) {
 		throw ExcecaoListaVazia();
 	}
-	saiu = cabeca;
-	volta = saiu->getInfo();
+	Elemento<T>* saiu = cabeca;
+	T volta = saiu->getInfo();
 	cabeca = saiu->getProximo();
 	tamanho--;
 	delete saiu;
 	return volta;
+}
+
+template <typename T>
+T ListaEnc<T>::retiraEspecifico(const T& dado) {
+	if(listaVazia()) {
+		throw ExcecaoListaVazia();
+	}
+	return retiraDaPosicao(posicao(dado) + 1);
 }
 
 template <typename T>
